cd4b880ee7969baf9137c7307e5b2c7d19b56c5e
palakpadalia <padaliapalak19@gmail.com>
2023-03-29 19:19:47 +0530

    Create Doctype

---
+++ indiamart/indiamart/doctype/indiamart_api/indiamart_api.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Dhruvi and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Indiamart API', {
+	// refresh: function(frm) {
+
+	// }
+});
